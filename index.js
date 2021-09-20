ea247deb578fd8523228884f657bde71b13f6dfb
SnowBit <65452005+snowbit-coderboi@users.noreply.github.com>
2021-09-20 09:46:05 +0530

    Created basic logic

---
+++ index.js
@@ -0,0 +1,17 @@
+function sendMessage() {
+  const request = new XMLHttpRequest();
+  request.open(
+    "POST",
+    "https://discord.com/api/webhooks/889363795053518848/S3RZcwyPy3_7cX1XWOEl7pUM5CU-Qj-CVPDOAVCTJzIJZfvEwQrrCj_oh27Dp8mjW2uc"
+  );
+
+  request.setRequestHeader("Content-type", "application/json");
+
+  const params = {
+    username: "Nokohashi",
+    avatar_url: "",
+    content: "Lol"
+  };
+
+  request.send(JSON.stringify(params));
+}
